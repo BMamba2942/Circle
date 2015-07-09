a340b41630e6c536a1e46db978535b3c473cb925
ElectroCamel <fortino.general@gmail.com>
2015-07-09 21:00:53 +0200

    Fixed three errors
    Error 1: Circle.java: line 8: typo: "ass" instead of "class" (I see what
    you did there..)
    Error 2: Circle.java: line 40: typo: "MaNy" instead of "many"
    Error 3: Run_Circle: line 9: missing "new"
    
    Also added a comment

--- src/Circle.java
+++ src/Circle.java
@@ -1,33 +1,35 @@
 //Read this first please
 
 //Author: HappyZombies
-/*Taken from tutorial online, comments added so I myself could understand 
+/*Taken from tutorial online, comments added so I myself could understand
 what is going on :) */
 
 //Define the class
-public ass Circle {   //whatever we call the file, this class must be the same
-	
+public class Circle {   //whatever we call the file, this class must be the same
+
 	private double radius;
 	private String color;
-	
+
 	//Constructors (they are overloaded!)
 	//Remember: Constructors do not return values, unlike methods
-	
+
 	/*Also, you should name your constructors the same as your class, capitalize letter
 	and all*/
-	
+
+	/* Actually, you have to ;) */
+
 	public Circle(){   //~~~1ST CONSTRUCTOR~~~
 		radius = 1.0;
 		color = "red";
-		/*we assigned variables at the top, now for this constructor, we 
+		/*we assigned variables at the top, now for this constructor, we
 		are manually putting values in it*/
 	}
 	public Circle(double r){  //~~~2ND CONSTRUCTOR~~~
 		radius = r;
 		color = "red";
-		/*we tell radius now equals r. 
-		r is not private. So now, it will allow us to enter data through 'r'. 
-		 
+		/*we tell radius now equals r.
+		r is not private. So now, it will allow us to enter data through 'r'.
+
 		Remember: Private only allows THIS class (called class Circle) to 'read it'*/
 	}
 	public Circle(double radius, String color){ //~~~3RD CONSTRUCTOR~~~
@@ -37,12 +39,12 @@ public ass Circle {   //whatever we call the file, this class must be the same
 		Here we used this. so we could understand how it works:)
 		Really, it's best to call the variables the same, but since we have only two
 		it's okay to call them r & c, but when you have A LOt of variables, r & c could 
-		mean MaNy things*/
+		mean many things*/
 	}
-	
-	
-	
-	//Public methods. Methods RETURN a value. 
+
+
+
+	//Public methods. Methods RETURN a value.
 	public double getRadius(){
 		return radius;
 		/*right now , radius equals 0 because we didn't give it a value (see private var).
@@ -50,16 +52,14 @@ public ass Circle {   //whatever we call the file, this class must be the same
 		'radius' and 'color'*/
 	}
 	public String getColor(){
-		return color; 
-		/*Remember: make sure the public String getColor() matches the data type you gave 
+		return color;
+		/*Remember: make sure the public String getColor() matches the data type you gave
 		 it in the beginning*/
 	}
 	public double getArea(){
 		return radius*radius*Math.PI;
 		//here, we return a mathematical expression
 	}
-	
+
 	//let's head over to the Run_Circle.java
 }
-
-
--- src/Run_Circle.java
+++ src/Run_Circle.java
@@ -1,35 +1,35 @@
 //Run the program !
 public class Run_Circle {
-	public static void main(String[] args){ 
+	public static void main(String[] args){
 		/*here, we tell Java to get 'Circle' (which is in the other file)
 		 we then give it a variable, it can be anything, but it's best to call
 		 it an appropriate name. Don't want to call it 'Circle spongebob = new Circle();
 		 We call it c1 because it is the first constructor that we will use!
 		 */
-		Circle c1 = Circle();
+		Circle c1 = new Circle();
 		System.out.println("Radius is " + c1.getRadius()+ " Color is " + c1.getColor() + " Area is "+ c1.getArea());
 		/*
 		 A system println is done.
-		 We use the dot operator to invoke the methods! Remember the methods? They return 
-		 the radius....color...and area! 
-		 
+		 We use the dot operator to invoke the methods! Remember the methods? They return
+		 the radius....color...and area!
+
 		 Our first constructor already have values , they equaled to 1.0 and 'red'
 		 */
-		
+
 		Circle c2 = new Circle(2.0);
 		System.out.println("Radius is " + c2.getRadius()+ " Color is " + c2.getColor() + " Area is "+ c2.getArea());
 		/*
 		 * Observe, Constructor 2 needed data for it's double r. Here, we tell it (give r value 2.0)
 		 * r now equals 2.0, and that means that radius now equals 2.0!
 		 */
-		
+
 		Circle c3 = new Circle(2.0, 'blue');
-			System.out.println("Radius is " + c3.getRadius()+ " Color is " + c3.getColor() + " Area is "+ c3.getArea());
-			/*
-			 * Constructor 3 needed 2 different data values, so we gave it to them
-			 * 
-			 * 
-			 * We are done ! Yay!!!
-			 */
+		System.out.println("Radius is " + c3.getRadius()+ " Color is " + c3.getColor() + " Area is "+ c3.getArea());
+		/*
+		 * Constructor 3 needed 2 different data values, so we gave it to them
+		 *
+		 *
+		 * We are done ! Yay!!!
+		 */
 	}
 }
